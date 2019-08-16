ee6c2d9fde78ca559b1c08290db00ba58f8b2733
GyuReeKim <starkim96@naver.com>
2019-08-16 16:58:09 +0900

    algorithm 0816

--- August/190816_03.py
+++ August/190816_03.py
@@ -1,12 +1,3 @@
-# import pprint
-# 회문인가?
-
-# 길이가 5인 회문이 존재하는가?
-# 길이가 4인 회문이 존재하는가
-# 길이가 3인 회문이 존재하는가?
-
-# palindrome
-
 T = int(input())
 for tc in range(1, T+1):
 
@@ -18,8 +9,6 @@ for tc in range(1, T+1):
         for j in range(N):
             arr[i][j] = chars[j]
 
-    # pprint.pprint(arr, indent=4, width=200)
-
     for i in range(N):
         row = ''
         col = ''
@@ -27,16 +16,16 @@ for tc in range(1, T+1):
             row += arr[i][j]
             col += arr[j][i]
 
-        print(row)
+        # print(row)
         for r in range(N-M+1):
             pal_r = row[r: r+M]
             if pal_r == pal_r[::-1]:
                 print('#{} {}'.format(tc, pal_r))
-                break
+                # break
 
-        print(col)
+        # print(col)
         for c in range(N-M+1):
-            pal_c = col[r: r+M]
+            pal_c = col[c: c+M]
             if pal_c == pal_c[::-1]:
                 print('#{} {}'.format(tc, pal_c))
-                break
+                # break
